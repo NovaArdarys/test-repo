857db3336b49bc9b21125bab987720311d263d71
Nova <alexis.nova.hartley@gmail.com>
2025-10-30 05:09:54 +0700

    delete unused

--- packages/storage-service/src/utils/minioClient.ts
+++ packages/storage-service/src/utils/minioClient.ts
@@ -83,7 +83,6 @@ export async function uploadToMinio(
   await minioClient.putObject(bucket, fileName, nodeStream, undefined, {
     "Content-Type": contentType,
   });
-  http://128.199.77.145:9000/
   const endpoint = process.env.MINIO_ENDPOINT || "127.0.0.1";
   const port = process.env.MINIO_PORT || "9000";
   const protocol = process.env.MINIO_USE_SSL === "true" ? "https" : "http";
