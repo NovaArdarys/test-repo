f161af8fbed05e5bc8d1b777b4bc19d12bbeffd3
Nova <alexis.nova.hartley@gmail.com>
2025-11-04 21:24:37 +0700

    update enum

---
+++ packages/database/drizzle/0045_rare_misty_knight.sql
@@ -0,0 +1,2 @@
+CREATE TYPE "public"."role_domain" AS ENUM('kitchen', 'beneficiary', 'driver', 'app_manager', 'other');--> statement-breakpoint
+ALTER TABLE "roles" ADD COLUMN "domain" "role_domain" DEFAULT 'other' NOT NULL;
