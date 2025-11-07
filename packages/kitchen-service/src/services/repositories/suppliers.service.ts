e7de5e8bc1d776bb64aee06a3dc216f9e7f210e5
Nova <alexis.nova.hartley@gmail.com>
2025-11-07 13:01:58 +0700

    add supplier product

--- packages/kitchen-service/src/services/repositories/suppliers.service.ts
+++ packages/kitchen-service/src/services/repositories/suppliers.service.ts
@@ -22,8 +22,13 @@ export async function getSuppliers({
   const normalizedSearch = search?.toLowerCase().trim();
   const keywords = normalizedSearch ? normalizedSearch.split(/\s+/) : [];
 
+  // const baseCondition = and(
+  //   kitchenIds.length > 0 ? inArray(suppliers.kitchenId, kitchenIds) : undefined,
+  //   eq(suppliers.isDeleted, false),
+  //   eq(suppliersProducts.isDeleted, false)
+  // );
+
   const baseCondition = and(
-    kitchenIds.length > 0 ? inArray(suppliers.kitchenId, kitchenIds) : undefined,
     eq(suppliers.isDeleted, false),
     eq(suppliersProducts.isDeleted, false)
   );
