273ea7000b06e533153f43d8ec8c75509a94fede
Nova <alexis.nova.hartley@gmail.com>
2025-11-14 04:00:32 +0700

    done remove unused column

---
+++ packages/database/drizzle/0007_long_slyde.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "delivery_beneficiaries" DROP COLUMN "status";--> statement-breakpoint
+ALTER TABLE "delivery_beneficiaries" DROP COLUMN "delivered_at";--> statement-breakpoint
+ALTER TABLE "delivery_beneficiaries" DROP COLUMN "notes";
