4a8bdc3fdfa172af516a80e58d3c2991dfb0dd98
Nova <alexis.nova.hartley@gmail.com>
2025-11-13 21:56:34 +0700

    add dev mode

--- packages/menu-service/src/index.ts
+++ packages/menu-service/src/index.ts
@@ -58,7 +58,10 @@ const app = new Hono<{ Variables: Variables; }>()
       return filePath;
     }
   }))
-  .get('/swagger/mobile', swaggerUI({ url: '/menu/api/mobile/openapi.json' }))
+  .get('/swagger/mobile', swaggerUI({
+    url: process.env.ENVIRONMENT === 'DEVELOPMENT'
+      ? '/api/mobile/openapi.json' : '/menu/api/mobile/openapi.json'
+  }))
   .get('/swagger', swaggerUI({ url: '/menu/api/openapi.json' })).get('/api/health', async (c) => {
     const dbStatus = await checkDatabase();
     const rabbitStatus = await checkBroker();
