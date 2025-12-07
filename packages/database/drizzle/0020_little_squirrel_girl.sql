84f3f2ae7a87ca62f383234a540fb36f448d3d94
Nova <alexis.nova.hartley@gmail.com>
2025-12-08 05:32:18 +0700

    done migrations

--- packages/database/drizzle/0020_little_squirrel_girl.sql
+++ packages/database/drizzle/0020_little_squirrel_girl.sql
@@ -1,17 +1,4 @@
-ALTER TABLE "master_steps"
-ALTER COLUMN "sub_domains"
-SET DATA TYPE text[]
-USING "sub_domains"::text[];
-
-ALTER TABLE "master_steps"
-ALTER COLUMN "sub_domains"
-SET DEFAULT '{}';
-
-ALTER TABLE "roles"
-ALTER COLUMN "sub_domains"
-SET DATA TYPE text[]
-USING "sub_domains"::text[];
-
-ALTER TABLE "roles"
-ALTER COLUMN "sub_domains"
-SET DEFAULT '{}';
+ALTER TABLE "master_steps" ALTER COLUMN "sub_domains" SET DATA TYPE text[];--> statement-breakpoint
+ALTER TABLE "master_steps" ALTER COLUMN "sub_domains" SET DEFAULT '{}';--> statement-breakpoint
+ALTER TABLE "roles" ALTER COLUMN "sub_domains" SET DATA TYPE text[];--> statement-breakpoint
+ALTER TABLE "roles" ALTER COLUMN "sub_domains" SET DEFAULT '{}';
