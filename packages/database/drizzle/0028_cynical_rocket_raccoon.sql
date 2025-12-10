29d9a8eedb2473fbe5f348f2d76e3eae57c0b4ae
Nova <alexis.nova.hartley@gmail.com>
2025-12-10 13:05:48 +0700

    generate schema

---
+++ packages/database/drizzle/0028_cynical_rocket_raccoon.sql
@@ -0,0 +1,2 @@
+ALTER TYPE "public"."step_key_enum" ADD VALUE 'inspection_before' BEFORE 'distribution';--> statement-breakpoint
+ALTER TYPE "public"."step_key_enum" ADD VALUE 'inspection_after' BEFORE 'distribution';
