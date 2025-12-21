5b67df9f5b9874964217a96f94d07c971cb3c7cd
Nova <alexis.nova.hartley@gmail.com>
2025-12-21 15:47:27 +0700

    update schema

---
+++ packages/database/drizzle/0036_tense_terror.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "menu_plan_beneficiaries" ADD COLUMN "lon" numeric(10, 6) DEFAULT '0';--> statement-breakpoint
+ALTER TABLE "menu_plan_beneficiaries" ADD COLUMN "lat" numeric(10, 6) DEFAULT '0';
